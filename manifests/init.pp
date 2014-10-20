768f4942bf644e1c3c25c31cb16546def5668ca9
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 23:49:44 -0400

    revert

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,19 @@
 class drobo_dashboard {
   package { 'DroboDashboard':
     ensure   => installed,
-    provider => 'pkgdmg',
+    provider => 'appdmg',
     source   => 'http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-2.6.3.dmg'
+  }
+
+  exec {'run the installer':
+    command => '/usr/bin/open /Applications/Install.app',
+    require => Package['DroboDashboard'],
+    onlyif  => 'test ! /Applications/Drobo\ Dashboard.app'
+  }
+
+  exec {'remove the (un)installer':
+    command => '/bin/rm -rf /Applications/Install.app /Applications/Uninstall.app',
+    require => Package['DroboDashboard'],
+    onlyif  => 'test -d /Applications/Install.app && test -d /Applications/Uninstall.app && test -d /Applications/Drobo\ Dashboard.app'
   }
 }
