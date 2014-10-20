fa451f4276db92f0fa2e5b5c67d94622e5b4b094
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 23:41:46 -0400

    try pkgdmg

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,19 +6,7 @@
 class drobo_dashboard {
   package { 'DroboDashboard':
     ensure   => installed,
-    provider => 'appdmg',
+    provider => 'pkgdmg',
     source   => 'http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-2.6.3.dmg'
-  }
-
-  exec {'run the installer':
-    command => '/usr/bin/open /Applications/Install.app',
-    require => Package['DroboDashboard'],
-    onlyif  => 'test ! /Applications/Drobo\ Dashboard.app'
-  }
-
-  exec {'remove the (un)installer':
-    command => '/bin/rm -rf /Applications/Install.app /Applications/Uninstall.app',
-    require => Package['DroboDashboard'],
-    onlyif  => 'test -d /Applications/Install.app && test -d /Applications/Uninstall.app && test -d /Applications/Drobo\ Dashboard.app'
   }
 }
